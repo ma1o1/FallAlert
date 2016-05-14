e6720300a93a5779f5d5dcc7737ea1558e3850ff
Matevz Peternel <matevz.peternel@yahoo.com>
2016-05-14 14:29:56 +0000

    Json respose

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 //We need a function which handles requests and send response
 function handleRequest(request, response){
     response.end('It Works!! Path Hit: ' + request.url);
+    
+    if(request.url=='/burek'){
+        console.log("Request handler random was called.");
+         response.writeHead(200, {"Content-Type": "application/json"});
+        var otherArray = ["item1", "item2"];
+          var otherObject = { item1: "item1val", item2: "item2val" };
+          var json = JSON.stringify({ 
+            anObject: otherObject, 
+            anArray: otherArray, 
+            another: "item"
+          });
+          response.end(json);
+
+    }
 }
 
 //Create a server
